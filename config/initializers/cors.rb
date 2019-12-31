2360ebde1dc89d90ed519c9b32bb04a8f9bf269e
Ruto Ken <ruto.k.ken@gmail.com>
2019-12-31 17:16:28 +0300

    cors setup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,7 +18,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
     if Rails.env.production?
-      origins 'https://dairycow.herokuapp.com'
+      origins 'https://dairycowapi.herokuapp.com'
     else
      origins 'http://localhost:3000'
    end
